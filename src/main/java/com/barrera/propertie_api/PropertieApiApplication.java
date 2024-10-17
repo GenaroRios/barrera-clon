9102dc06ae27eba476476066a468396b9473c1f1
GenaroRios <genarios2002@gmail.com>
2024-10-17 10:03:52 -0300

    First commit

---
+++ src/main/java/com/barrera/propertie_api/PropertieApiApplication.java
@@ -0,0 +1,13 @@
+package com.barrera.propertie_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertieApiApplication.class, args);
+	}
+
+}
